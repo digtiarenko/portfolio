d77d72c5d87d6e60a3c905dba91e1fe22efa838d
Valentyn Digtiarenko <baggins999@gmail.com>
2022-11-26 17:48:42 +0200

    working on homepage

--- src/components/home/Data.jsx
+++ src/components/home/Data.jsx
@@ -5,14 +5,14 @@ import { ReactComponent as SendSVG } from '../../assets/send.svg';
 const Data = () => {
    return (
       <div className="home__data">
-         <h1 className="home__data">
+         <h1 className="home__title">
             Valentyn Digtiarenko <HandSVG />
          </h1>
-         <h3 className="home__subtitle">_Full-Stack developer</h3>
+         <h3 className="home__subtitle">Full Stack Developer</h3>
          <p className="home_description">
-            Passionate and experienced media guy goes IT. Bringing forth 10+
-            years experience of communication, planning and coordination skills
-            gained on editors and directors positions.
+            Passionate and experienced media guy goes IT. Bringing forth 10
+            years of communication, planning and coordination skills gained on
+            editors and directors positions.
          </p>
 
          <a href="#contact" className="button button--flex">
