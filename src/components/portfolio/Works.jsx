f7bcb6edbe68f580a009af505ee3e03bef2f36be
Valentyn Digtiarenko <baggins999@gmail.com>
2022-12-09 14:58:20 +0200

    portfolio styles

--- src/components/portfolio/Works.jsx
+++ src/components/portfolio/Works.jsx
@@ -1,22 +1,50 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
 import { projectData, projectNav } from './Data';
 import WorksItems from './WorksItems';
 
 const Works = () => {
+   const [item, setItem] = useState('all');
+   const [projects, setProjects] = useState([]);
+   const [active, setActive] = useState(0);
+
+   const handleClick = (e, index) => {
+      setItem(e.target.textContent);
+      setActive(index);
+   };
+
+   useEffect(() => {
+      if (item === 'all') {
+         setProjects(projectData);
+      } else {
+         const newProjects = projectData.filter(
+            project => project.category === item,
+         );
+         setProjects(newProjects);
+      }
+   }, [item]);
+
    return (
       <>
-         <div className="work__filter">
+         <div className="portfolio__filter">
             {projectNav.map((item, index) => {
                return (
-                  <span key={index} className="work__item">
+                  <span
+                     onClick={e => {
+                        handleClick(e, index);
+                     }}
+                     key={index}
+                     className={`${
+                        active === index ? 'active-work' : ''
+                     } portfolio__item`}
+                  >
                      {item.name}
                   </span>
                );
             })}
          </div>
 
-         <div className="work__container container grid">
-            {projectData.map(item => (
+         <div className="portfolio__container container grid">
+            {projects.map(item => (
                <WorksItems item={item} key={item.id} />
             ))}
          </div>
--- src/components/portfolio/WorksItems.jsx
+++ src/components/portfolio/WorksItems.jsx
@@ -1,10 +1,11 @@
 const WorksItems = ({ item }) => {
    return (
-      <div className="work__card" key={item.id}>
-         <img src={item.image} alt={item.title} className="work__img" />
-         <h3 className="work__title">{item.title}</h3>
-         <a href="#" className="work__button">
-            Demo <i className="bx bx-right-arrow-alt"></i>
+      <div className="portfolio__card" key={item.id}>
+         <img src={item.image} alt={item.title} className="portfolio__img" />
+         <h3 className="portfolio__title">{item.title}</h3>
+         <a href="#" className="portfolio__button">
+            Demo
+            <i className="bx portfolio__button-icon bx-right-arrow-alt"></i>
          </a>
       </div>
    );
