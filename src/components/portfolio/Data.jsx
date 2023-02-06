dd9cb43b3edfdd9c81d504c3870d3c76ea4eeec1
Valentyn Digtiarenko <baggins999@gmail.com>
2023-02-06 14:10:37 +0200

    added space news

--- src/components/portfolio/Data.jsx
+++ src/components/portfolio/Data.jsx
@@ -2,6 +2,7 @@ import Work1 from '../../assets/work1.jpg';
 import Work2 from '../../assets/work2.jpg';
 import Work3 from '../../assets/work3.jpg';
 import Work4 from '../../assets/work4.jpg';
+import Work5 from '../../assets/work5.jpg';
 
 export const projectData = [
    {
@@ -20,17 +21,17 @@ export const projectData = [
       title: 'Kapusta: smart finance',
       category: 'fullstack',
       description:
-         'A multipage app for managing your savings. \nTechnologies: React, Redux, Node.js, MongoDB, Google authentication, Swagger, Formik ect.',
+         'A SPA for managing your savings. \nTechnologies: React, Redux, Node.js, MongoDB, Google authentication, Swagger, Formik ect.',
       link: 'https://kapustaapp.netlify.app/',
    },
    {
       id: 3,
-      image: Work3,
-      title: 'ChatApp',
-      category: 'back',
+      image: Work5,
+      title: 'Space NEWS',
+      category: 'front',
       description:
-         'A responsive React messaging app based on socket.io with authentication, individual/group chat functionality, chat editing, notification ect. \nTechnologies: React, Node.js, Socket.io, MongoDB, Firebase.',
-      link: 'https://fast-journey-62386.herokuapp.com/',
+         'A fully responsive and easy-to-use and convenient platform for people to stay informed \nabout the latest developments in space exploration, science, and technology. \nTechnologies: TS, HTML/SASS,  REST APIs, MUI, Redux Toolkit',
+      link: 'https://gilded-douhua-d195e0.netlify.app/',
    },
    {
       id: 4,
@@ -40,6 +41,15 @@ export const projectData = [
          'MPA with basic contact book functionality. \nReact + Material.ui. Redux + Redux Toolkit + RTK Query serves as state manager. WebServer [Node.js + Express] in connection with MongoDB.',
       category: 'fullstack',
       link: 'https://marvelous-bunny-daf51a.netlify.app/',
+   },
+   {
+      id: 5,
+      image: Work3,
+      title: 'ChatApp',
+      category: 'back',
+      description:
+         'A responsive React messaging app based on socket.io with authentication, individual/group chat functionality, chat editing, notification ect. \nTechnologies: React, Node.js, Socket.io, MongoDB, Firebase.',
+      link: 'https://fast-journey-62386.herokuapp.com/',
    },
 ];
 
