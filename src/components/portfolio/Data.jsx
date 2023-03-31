677abf5ec4002683c9d9897f67d99bc5729eb241
Valentyn Digtiarenko <baggins999@gmail.com>
2023-03-31 14:40:31 +0300

    added github link and rick&morty project

--- src/components/portfolio/Data.jsx
+++ src/components/portfolio/Data.jsx
@@ -1,8 +1,9 @@
 import Work1 from '../../assets/work1.jpg';
 import Work2 from '../../assets/work2.jpg';
 import Work3 from '../../assets/work3.jpg';
-import Work4 from '../../assets/work4.jpg';
+// import Work4 from '../../assets/work4.jpg';
 import Work5 from '../../assets/work5.jpg';
+import Work6 from '../../assets/work6.jpg';
 
 export const projectData = [
    {
@@ -14,6 +15,7 @@ export const projectData = [
          'A webpage with a responsive layout based on vanilla JS and themoviedb-api. Includes color switcher and personal library.\nTechnologies: JS, HTML/SASS,  REST APIs, Handlebars',
 
       link: 'https://digtiarenko.github.io/JS-movie_api/',
+      github: 'https://github.com/digtiarenko/JS-movie_api',
    },
    {
       id: 2,
@@ -23,6 +25,7 @@ export const projectData = [
       description:
          'A SPA for managing your savings. \nTechnologies: React, Redux, Node.js, MongoDB, Google authentication, Swagger, Formik ect.',
       link: 'https://kapustaapp.netlify.app/',
+      github: 'https://github.com/digtiarenko/kapusta_app_frontend',
    },
    {
       id: 3,
@@ -32,16 +35,18 @@ export const projectData = [
       description:
          'A fully responsive and easy-to-use and convenient platform for people to stay informed \nabout the latest developments in space exploration, science, and technology. \nTechnologies: TS, HTML/SASS,  REST APIs, MUI, Redux Toolkit',
       link: 'https://gilded-douhua-d195e0.netlify.app/',
+      github: 'https://github.com/digtiarenko/star_news',
    },
-   {
-      id: 4,
-      image: Work4,
-      title: 'Contact book',
-      description:
-         'MPA with basic contact book functionality. \nReact + Material.ui. Redux + Redux Toolkit + RTK Query serves as state manager. WebServer [Node.js + Express] in connection with MongoDB.',
-      category: 'fullstack',
-      link: 'https://marvelous-bunny-daf51a.netlify.app/',
-   },
+   // {
+   //    id: 4,
+   //    image: Work4,
+   //    title: 'Contact book',
+   //    description:
+   //       'MPA with basic contact book functionality. \nReact + Material.ui. Redux + Redux Toolkit + RTK Query serves as state manager. WebServer [Node.js + Express] in connection with MongoDB.',
+   //    category: 'back, fullstack',
+   //    link: 'https://marvelous-bunny-daf51a.netlify.app/',
+   //    github: 'https://github.com/digtiarenko/nodeJS_REST_api',
+   // },
    {
       id: 5,
       image: Work3,
@@ -50,6 +55,17 @@ export const projectData = [
       description:
          'A responsive React messaging app based on socket.io with authentication, individual/group chat functionality, chat editing, notification ect. \nTechnologies: React, Node.js, Socket.io, MongoDB, Firebase.',
       link: 'https://fast-journey-62386.herokuapp.com/',
+      github: 'https://github.com/digtiarenko/chat_app',
+   },
+   {
+      id: 6,
+      image: Work6,
+      title: 'Rick&Morty',
+      category: 'front',
+      description:
+         'This project is a modern and minimal Vite + React + TypeScript template. \nFor state management and data caching, RTK Query is used including newly added transformResponse and merge funtionality.',
+      link: 'https://musical-medovik-5ae0ad.netlify.app/',
+      github: 'https://github.com/digtiarenko/rick-morty',
    },
 ];
 
--- src/components/portfolio/WorksItems.jsx
+++ src/components/portfolio/WorksItems.jsx
@@ -3,11 +3,29 @@ const WorksItems = ({ item }) => {
       <div className="portfolio__card" key={item.id}>
          <img src={item.image} alt={item.title} className="portfolio__img" />
          <h3 className="portfolio__title">{item.title}</h3>
-         <p className="portfolio__description">{item.description}</p>
-         <a href={item.link} target="_blank" className="portfolio__button">
-            Demo
-            <i className="bx portfolio__button-icon bx-right-arrow-alt"></i>
-         </a>
+         <div className="portfolio__text-wrappe">
+            <p className="portfolio__description">{item.description}</p>
+            <div className="link__wrapper">
+               <a
+                  href={item.link}
+                  target="_blank"
+                  className="portfolio__button"
+                  rel="noreferrer"
+               >
+                  webpage
+                  <i className="bx portfolio__button-icon bx-right-arrow-alt"></i>
+               </a>
+               <a
+                  href={item.github}
+                  target="_blank"
+                  className="portfolio__button"
+                  rel="noreferrer"
+               >
+                  github
+                  <i className="bx portfolio__button-icon bx-right-arrow-alt"></i>
+               </a>
+            </div>
+         </div>
       </div>
    );
 };
